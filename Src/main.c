0394c05b9f141c8ebb5af7ac474005e291094d46
Erik Karlsson <erik.johan.karlsson1@gmail.com>
2020-10-27 16:18:28 +0100

    Hello World

---
+++ Src/main.c
@@ -0,0 +1,10 @@
+#include "common.h"
+#include "hash_hable.h"
+#include "list_linked.h"
+#include "stdio.h"
+#include "utils.h"
+
+int main(int argc, char **argv) {
+  printf("Hello world!");
+  return 0;
+}
