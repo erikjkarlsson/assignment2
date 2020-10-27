5ff06ef9eb307881e4913e08739a5e7adedea070
Maja4713 <Maria.Jan.4713@student.uu.se>
2020-10-27 16:00:06 +0100

    Header functions

--- webstore.h
+++ webstore.h
@@ -12,5 +12,45 @@
 void add_merchandise();
 
 //This should list all items in the store.
-
+//Items should preferably (soft requirement) be printed in alphabetical order on their names.
+//Because there may be more things in the database than might fit on a screen,
+//items should be printed 20 at a time, and the user is asked to continue listing (if possible)
+//or return to the main menu.
 void list_merchandise();
+
+//Removes an item completely from the warehouse, including all its stock.
+void  remove_merchandise();
+
+//Allows changing the name, description and price of a merch. Note that this does not affect its stock.
+//Changing the name of a merch to the name of an existing merch is not allowed.
+//Note that changing the name may mean changing the key unless you use a unique id for each merch.
+void edit_merchandise();
+
+//List all the storage locations for a particular merch,
+//along with the quantities stored on each location.
+//Storage locations should preferably be listed in alphabetical order (e.g., A20 before B01 and C01 before C10).
+//Names of storage locations follow this format always: one capital letter (A-Z) followed by two digits (0-9).
+void show_stock();
+
+//Increases the stock of a merch by at least one.
+//You can replenish on an existing storage location or a new one.
+//The stock for a merch is the sum of all items on all storage locations holding that merch.
+//A storage location stocks items of one (type of) merch, never more.
+//For simplicity, there is no limit to the amount of storage locations nor is there a limit on the number of items a location can hold.
+void replenish();
+
+void create_cart();
+
+void remove_cart();
+
+void add_to_cart();
+
+void remove_from_cart();
+
+void calculate_cost();
+
+void checkout();
+
+void undo();
+
+void Quit();
