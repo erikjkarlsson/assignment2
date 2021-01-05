d111df21ac2b912a038078ba9be4a1b79d7ba015
Erik Karlsson <hotdogwizard@gitlab.com>
2021-01-05 16:07:36 +0100

    Added some tests

--- Src/test.c
+++ Src/test.c
@@ -4,6 +4,9 @@
 #include <CUnit/Basic.h>
 #include <stdlib.h>
 #include "hash_table.h"
+#include "list_linked.h"
+#include "webstore.h"
+#include "common.h"
 
 
 int init_suite(void)
@@ -23,7 +26,47 @@ void test(){
     CU_ASSERT_TRUE(true);
 }
 /////////////////////////////////////////////////////////////
+void test_merch(){
+  webstore_t *store = store_create();
+  
+  add_merchendise(store, "Bike", "A sports bike from Brazil",
+		  (size_t)4);
+  add_merchendise(store, "Car", "A fast car",
+		  (size_t)2);
 
+  // --- Bike
+  CU_ASSERT_TRUE(merch_in_stock(store, "Bike"));
+    
+  CU_ASSERT_TRUE(STR_EQ(merch_description(store, "Bike"),
+			"A sports bike from Brazil"));
+
+  CU_ASSERT_TRUE(merch_price(store, "Bike") == (size_t)4);
+
+  // --- Car
+  CU_ASSERT_TRUE(merch_in_stock(store, "Car"));
+  
+  CU_ASSERT_TRUE(STR_EQ(merch_description(store, "Car"),
+			"A fast car"));
+  CU_ASSERT_TRUE(merch_price(store, "Car") == (size_t)2);
+  
+  remove_merchendise(store, "Car");
+  remove_merchendise(store, "Bike");
+  
+  store_destroy(store);
+}
+/////////////////////////////////////////////////////////////
+void test_storage(){
+  
+}
+/////////////////////////////////////////////////////////////
+void test_locs(){
+  
+}
+/////////////////////////////////////////////////////////////
+void test_sync(){
+  
+}
+/////////////////////////////////////////////////////////////
 int main()
 {
   CU_pSuite test_suite1 = NULL;
@@ -31,18 +74,16 @@ int main()
   if (CUE_SUCCESS != CU_initialize_registry())
     return CU_get_error();
 
-  test_suite1 = CU_add_suite("Test Suite 1", init_suite, clean_suite);
-  if (NULL == test_suite1)
-  
-    {
+  test_suite1 = CU_add_suite("Tests API", init_suite, clean_suite);
+  if (NULL == test_suite1){
       CU_cleanup_registry();
       return CU_get_error();
-    }
+  }
 
-  if (
-    (NULL == CU_add_test(test_suite1, "test", test))
-  )
-    {
+  if ((NULL == CU_add_test(test_suite1, "Merch Test",   test_merch))   ||
+      (NULL == CU_add_test(test_suite1, "Storage Test", test_storage)) ||
+      (NULL == CU_add_test(test_suite1, "Locs Test",    test_locs))    ||
+      (NULL == CU_add_test(test_suite1, "Sync Test",    test_sync))){
       CU_cleanup_registry();
       return CU_get_error();
     }
