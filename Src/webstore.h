ea0f43430ee5ac611128c1dfe19f6007751f84d0
Erik Karlsson <hotdogwizard@gitlab.com>
2021-01-06 15:45:12 +0100

    Added remove_all_storage_locations to .h file

--- Src/webstore.h
+++ Src/webstore.h
@@ -142,7 +142,7 @@ void print_merch(merch_t *merch);
 char *lookup_merch_name(webstore_t *store, int index);
 
 ioopm_list_t *look_in_storage(webstore_t *store, char *shelf);
-
+void remove_all_storage_locations(webstore_t *store, char *shelf);
 ioopm_list_t *look_in_storage(webstore_t *store, char *shelf);
 void add_to_storage(webstore_t *store, char *name, char *shelf);
 void display_storage(webstore_t *store, char *shelf);
@@ -154,6 +154,7 @@ bool valid_index(webstore_t *store, int index);
 // Logging function
 // use macro: MLOG
 void merch_log(char *function, char *name, char *message, int number);
+
 
 
 //void locs_delete(ioopm_list_t *locs);
